bab81945c7475aec43c2c4fa21be3b664f1f8390
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-05-13 09:07:34 -0300

    insercao em arvores

--- 8 - arvores/arvore0.c
+++ 8 - arvores/arvore0.c
@@ -6,7 +6,6 @@
 /*
 As três maneiras mais usuais para percorrer os nós são (PROFUNDIDADE):
 
-
 Caminhamento Pré-fixado (RED)
     visita a raiz - printf, if, comando qualquer sobre o conteúdo do nodo
     percorre a sub-árvore da esquerda
@@ -29,57 +28,89 @@ typedef struct no {
     struct no *dir;
 } Arvore;
 
-//inserir
-Arvore *inserir(int valor, Arvore *r) { //ponto A - inicialização da variável de controle
-    if (r) { //ponto B - teste de parada
-        //avançar ou pra esquerda ou pra direita
+Arvore *inserir(int valor, Arvore *r) { //garante que a árvore esteja ordenada
+    if (r) {
         if (valor < r->conteudo) {
-            //ir para esquerda
-            r->esq = inserir(valor,r->esq);
+            //vai para esquerda
+            r->esq = inserir(valor, r->esq);
         } else {
-            //ir para direita
-            r->dir = inserir(valor, r->dir);
+            //vai para direita
+            r->dir = inserir(valor,r->dir);
         }
         return r;
     } else {
-        //toda a inserção ocorre nas folhas da árvore
-        Arvore *novo = (Arvore *)malloc(sizeof(Arvore));
+        Arvore *novo;
+        //alocar memoria
+        novo = (Arvore *)malloc(sizeof(Arvore));
+        //depositar valores
         novo->conteudo = valor;
         novo->esq = NULL;
         novo->dir = NULL;
+        //todo nó é inserido numa extremidade da árvore e ele torna-se folha
         return novo;
     }
 }
 
-//exibir
-
-void RED(Arvore *r) { //pesquisa
+//RED
+void RED(Arvore *r) {
     if (r) {
-        printf("%d\t", r->conteudo); //visita o raiz
-        RED(r->esq); //vai para esquerda
-        RED(r->dir); //vai para direita
+        printf("%d\t",r->conteudo); //visito o raiz
+        RED(r->esq);//vou pra esquerda
+        RED(r->dir);//vou pra direita
     }
 }
 
-void ERD(Arvore *r) { //ordem
-   if (r) {
-        ERD(r->esq); //vai para esquerda
-        printf("%d\t", r->conteudo); //visita o raiz
-        ERD(r->dir); //vai para direita
-    } 
+//ERD
+void ERD(Arvore *r) {
+    if (r) {
+        ERD(r->esq);
+        printf("%d\t",r->conteudo); //visito o raiz
+        ERD(r->dir);
+    }
 }
 
-void EDR(Arvore *r) { //podar/excluir/calcular altura
+//EDR
+void EDR(Arvore *r) {
     if (r) {
-        EDR(r->esq); //vai para esquerda
-        EDR(r->dir); //vai para direita
-        printf("%d\t", r->conteudo); //visita o raiz
-    } 
+        EDR(r->esq);
+        EDR(r->dir);
+        printf("%d\t",r->conteudo); //visito o raiz
+    }
 }
 
 //contar
+int contar(Arvore *r) {
+    if (r) {
+        return 1 + contar(r->esq) + contar(r->dir);
+    }
+    return 0;
+}
+
+//somar
+int somar(Arvore *r) {
+    if (r) {
+        return r->conteudo + somar(r->esq) + somar(r->dir);
+    }
+    return 0;
+}
 
 //localizar
+int localizar(int valor, Arvore *r) {
+    if (r) {
+        if (valor == r->conteudo) { //RED
+            return 1; //localizado
+        }
+        if (valor < r->conteudo) {
+            //ir para esquerda
+            return localizar(valor, r->esq);
+        } else {
+            //ir para direita
+            return localizar(valor, r->dir);
+        }
+    } else {
+        return 0; //nao localizado
+    }
+}
 
 //remover
 
@@ -93,47 +124,22 @@ void EDR(Arvore *r) { //podar/excluir/calcular altura
    43      200
 
 2       150
+    124
 */
 
-int contar(Arvore *r) {
-    if (r) {
-        return 1 + contar(r->esq) + contar(r->dir);
-    }
-    return 0;
-}
-
-int somar(Arvore *r) {
-    if (r) {
-        return r->conteudo + somar(r->esq) + somar(r->dir);
-    }
-    return 0;
-}
-
-int localizar(int valor, Arvore *r) {
-    if (r) {    
-        if (valor == r->conteudo) {
-            return 1; //localizado
-        }
-        if (valor < r->conteudo) {
-            //ir para esquerda
-            return localizar(valor, r->esq);
-        } else {
-            //ir para direita
-            return localizar(valor, r->dir);
-        }
-    }
-    return 0; //nao localizado
-}
 
 int main() {
     setlocale(LC_ALL,"Portuguese");
     printf("Aula sobre árvores encadeadas.....\n");
+    
     Arvore *raiz = NULL;
+
     raiz = inserir(124,raiz);
     raiz = inserir(43,raiz);
     raiz = inserir(2,raiz);
     raiz = inserir(200,raiz);
     raiz = inserir(150,raiz);
+    raiz = inserir(124,raiz);
 
 
     printf("A árvore possui %d elementos\n", contar(raiz));
