9b572766535726c8dd7037d245f320b0441fc64d
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2022-04-25 09:37:04 -0300

    discussão da avaiação

--- 2022/CC/revisao/arquivo3_leitura.c
+++ 2022/CC/revisao/arquivo3_leitura.c
@@ -1,3 +1,15 @@
+/**
+ * arquivo em disco precisa de uma variável em RAM que o represente, em geral um ponteiro para arquivo
+ * Operações:
+ *  - fopen - abrir (associar o arquivo ao procurador): read, append, write
+ *  - fclose - fechar (gravar os dados que estão em RAM para o Disco)
+ *  - fprintf: escrever no arquivo - gravar
+ *  - fscanf: ler do arquivo - ler variavel por variavel  
+ *  - fgets: ler do arquivo - ler uma linha inteira
+ *  - fgetc: ler do arquivo - ler caracter por caracter
+ *  - feof: função que verifica se o controlador do arquivo já chegou no final
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #define TAM 45
