7cc38fa4615402b6e2bfbb08ef5e628a056a1963
Your Name <you@example.com>
2023-03-27 20:12:36 -0300

    desafio glicose turma noite

--- _materialBase/4 - listas/desafioGlicemia/desafio.h
+++ _materialBase/4 - listas/desafioGlicemia/desafio.h
@@ -6,13 +6,140 @@ typedef struct no {
 } Celula;
 
 Celula *inserirOrdenado(int valor, string data, Celula *lista) {
-    return NULL;
+     //alocar memória
+    Celula *novo = (Celula *)calloc(1,sizeof(Celula));
+    Celula *p, *pR;
+
+    //depositar valores
+    novo->data = data;
+    novo->valor = valor;
+    novo->qtdOcorrencias = 1;
+    novo->prox = NULL;
+
+    //para a primeira alocação
+    if (!lista) return novo;
+
+    //definir a posicao do valor na lista, ou seja, percorrer a lista
+    for (pR = NULL, p = lista; p ; pR = p, p = p->prox) {
+        if (valor < p->valor) {
+            break;
+        }
+    }
+    //no inicio
+    if (p == lista) {
+        novo->prox = lista;
+        return novo;
+    }
+    //no fim
+    if (!p) {
+        pR->prox = novo;
+        return lista;
+    }
+    //no meio
+    pR->prox = novo;
+    novo->prox = p;
+    return lista;
 }
 
-Celula *inserirControleOcorrencia(int valor, Celula *lista) {
-    return NULL
+Celula *inserirControleDuplicado(int valor, string data, Celula *lista) {
+     //alocar memória
+    Celula *novo = (Celula *)calloc(1,sizeof(Celula));
+    Celula *p, *pR;
+
+    //depositar valores
+    novo->data = data;
+    novo->valor = valor;
+    novo->qtdOcorrencias = 1;
+    novo->prox = NULL;
+
+    //para a primeira alocação
+    if (!lista) return novo;
+
+    //definir a posicao do valor na lista, ou seja, percorrer a lista
+    for (pR = NULL, p = lista; p ; pR = p, p = p->prox) {
+        if (valor == p->valor) {
+            p->qtdOcorrencias++;
+            free(novo);
+            return lista;
+        }
+        
+        if (valor < p->valor) {
+            break;
+        }
+    }
+    //no inicio
+    if (p == lista) {
+        novo->prox = lista;
+        return novo;
+    }
+    //no fim
+    if (!p) {
+        pR->prox = novo;
+        return lista;
+    }
+    //no meio
+    pR->prox = novo;
+    novo->prox = p;
+    return lista;
 }
 
+Celula *copiaListaControleDuplicidade(Celula *lista) {
+    Celula *listaNova = NULL;
+    Celula *p;
+
+    for (p = lista; p; p = p->prox) {
+        listaNova = inserirControleDuplicado(p->valor, p->data, listaNova);
+    }
+    return listaNova;
+}
+
+Celula *ordenarPelasOcorrencias(Celula *lista) {
+    //usando o método da bolha ou bubble sort
+    Celula *tmp = (Celula*)calloc(1,sizeof(Celula));
+    Celula *p;
+    bool houveTroca;
+
+    do {
+        houveTroca = false;
+        for (p = lista; p->prox; p = p->prox) {
+            if (p->qtdOcorrencias < p->prox->qtdOcorrencias) {
+                houveTroca = true;
+
+                tmp->valor = p->valor;
+                tmp->data = p->data;
+                tmp->qtdOcorrencias = p->qtdOcorrencias;
+
+                p->valor = p->prox->valor;
+                p->data = p->prox->data;
+                p->qtdOcorrencias = p->prox->qtdOcorrencias;
+
+                p->prox->valor = tmp->valor;
+                p->prox->data = tmp->data;
+                p->prox->qtdOcorrencias = tmp->qtdOcorrencias;
+            }
+        }
+    } while (houveTroca);
+    free(tmp);
+    return lista;
+}
+
+
+void exibir(Celula *lista) {
+    Celula *p;
+    int i = 1;
+    for (p = lista; p; i++, p = p->prox) {
+        cout << i << "o " << p->valor << " - " << p->data << endl;
+    }
+}
+
+
+void exibirOcorrencias(Celula *lista) {
+    Celula *p;
+    int i = 1;
+    for (p = lista; p; i++, p = p->prox) {
+        cout << i << "o " << p->valor << " - " << p->qtdOcorrencias << endl;
+    }
+}
 
 int contarElementos(Celula *lista) {
     int qtd = 0;
@@ -24,15 +151,28 @@ int contarElementos(Celula *lista) {
 }
 
 float media(Celula *lista) {
-
-    return 1.0;
+    Celula *p;
+    int soma = 0;
+    for (p = lista; p; p = p->prox) {
+        soma += p->valor;
+    }
+    return soma / contarElementos(lista);
 }
 
 int mediana(Celula *lista) {
-    //se a estrutura for de tamanho impar, devolver o valor da glicemia que está no meio da lista ordenada
+    int quantidade = contarElementos(lista);
+    Celula *p;
+    int i;
+    for (i = 1, p = lista; i < quantidade / 2; i++, p = p->prox); //achar o valor do meio da lista ordenada
 
-    //se a estrutura for de tamanho par, devolver a media dos dois elementos mais ao centro da lista ordenada (parte inteira)
-    return 1; 
+    //se a estrutura for de tamanho impar, devolver o valor da glicemia que está no meio da lista ordenada
+    if (quantidade % 2 != 0) {
+        return p->valor;
+    }
+
+    //se for par retorna a media dos dois valores que estao ao centro       
+    return (int)(p->valor + p->prox->valor)/2;
+    
 }
 
 int min(Celula *lista) {
--- _materialBase/4 - listas/desafioGlicemia/desafio1.cpp
+++ _materialBase/4 - listas/desafioGlicemia/desafio1.cpp
@@ -6,9 +6,46 @@ using namespace std;
 
 #include "desafio.h"
 
+
+Celula *popularListaOrdenadaArquivo(Celula *lista) {
+    ifstream procurador;
+    string linha;
+    string data;
+    int valor; 
+
+    //o ideal seria o usuario informar o nome do arquivo com os dados de glicemia
+    cout << "Populando a lista...... \n";
+    procurador.open("dadosGlicose.txt");
+    do {
+        getline(procurador, linha);    
+        data = linha.substr(0, linha.find(" "));
+        valor = stoi(linha.substr(linha.find(" "), 100));
+        lista = inserirOrdenado(valor, data, lista);     
+    } while (!procurador.eof());
+
+
+    procurador.close();
+    return lista;
+}
+
 int main() {
     Celula *listaOrdenada = NULL; //nao precisa guardar a ocorrencia (0)
     Celula *listaControleOcorrencia = NULL; //nao precisa guardar a data (NULL)
+
+
+    listaOrdenada = popularListaOrdenadaArquivo(listaOrdenada);
+    cout << "Exibindo a lista com os valores glicemicos ordenados por valor\n";
+    exibir(listaOrdenada);
+    cout << "O total de amostras eh: " << contarElementos(listaOrdenada) << endl;
+
+    cout << "A media glicemica eh: " << media(listaOrdenada) << endl;
+    cout << "A mediana glicemica eh: " << mediana(listaOrdenada) << endl;
+
+
+    listaControleOcorrencia = copiaListaControleDuplicidade(listaOrdenada);
+    listaControleOcorrencia = ordenarPelasOcorrencias(listaControleOcorrencia);
+    cout << "Exibindo a lista sem valores duplicados\n";
+    exibirOcorrencias(listaControleOcorrencia);
     
     //menu
     return 1;
