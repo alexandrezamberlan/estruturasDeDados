39e6985a6c9db037ca8ed5344e89d03c5cb119a3
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2023-04-10 19:05:35 -0300

    codigos lista dupla turma noite

--- 2023_noite/util.h
+++ 2023_noite/util.h
@@ -100,6 +100,9 @@ void exibirListaSimples(Celula *lista) {
 
 bool localizarListaSimples(int valor, Celula *lista) {
     Celula *p;
+    
+    if (!lista) return false;
+
     for (p = lista; p; p=p->prox) {
         if (valor == p->valor) {
             return true;
@@ -115,5 +118,144 @@ Celula *atualizarListaSimples(int valorAntigo, int valorNovo, Celula *lista) {
         lista = inserirListaSimples(valorNovo, lista);
         cout << "valor atualizado com sucesso na lista\n";
     }
+    return lista;
+}
+
+//listas duplas
+
+typedef struct noD {
+    int valor;
+    struct no *prox, *ant;
+} CelulaD;
+
+void exibirListaDupla(CelulaD *lista) {
+    if (!lista) return;
+
+    //garatir que o controle da lista esteja no primeiro elemento
+    while (lista->ant) {
+        lista = lista->ant;
+    }
+
+    CelulaD *p;
+    for (p = lista; p; p = p->prox) {
+        cout << p->valor << "\t";
+    }
+    cout << endl;
+}
+
+void exibirListaDuplaDecrescente(CelulaD *lista) {
+    if (!lista) return;
+
+    //garatir que o controle da lista esteja no ultimo elemento
+    while (lista->prox) {
+        lista = lista->prox;
+    }
+
+    CelulaD *p;
+    for (p = lista; p; p = p->ant) {
+        cout << p->valor << "\t";
+    }
+    cout << endl;
+}
+
+bool localizarListaDupla(int valor, CelulaD *lista) {
+    CelulaD *p;
+
+    if (!lista) return false;
+
+    //garatir que o controle da lista esteja no primeiro elemento
+    while (lista->ant) {
+        lista = lista->ant;
+    }
+
+    for (p = lista; p; p=p->prox) {
+        if (valor == p->valor) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Celula *destruirListaDupla(CelulaD *lista) {
+    CelulaD *pR, *p;
+
+    if (!lista) return lista;
+
+    //garatir que o controle da lista esteja no primeiro elemento
+    while (lista->ant) {
+        lista = lista->ant;
+    }
+
+    for (pR = lista, p = lista->prox; p ; pR = p, p = p->prox) {
+        free(pR);    
+    }
+    free(pR);
+    return NULL;
+}
+
+Celula *destruirListaDupla2(CelulaD *lista) {
+    CelulaD *p;
+
+    if (!lista) return lista;
+
+    //garatir que o controle da lista esteja no primeiro elemento
+    while (lista->ant) {
+        lista = lista->ant;
+    }
+
+    if (!p->prox) {
+        free(p);
+        return NULL;
+    }
+
+    for (p = lista->prox; p->prox ; p = p->prox) {
+        free(p->ant);    
+    }
+    free(p->ant);
+    free(p);
+    return NULL;
+}
+
+Celula *inserirListaDupla(int valor, CelulaD *lista) {
+    //alocar memória
+    CelulaD *novo = (CelulaD *)malloc(sizeof(CelulaD));
+    CelulaD *p, *pR;
+
+    //depositar valores
+    novo->valor = valor;
+    novo->prox = NULL;
+
+    //para a primeira alocação
+    if (!lista) return novo;
+
+
+    //garantir que o controle da lista esteja no primeiro elemento
+    while (lista->ant) {
+        lista = lista->ant;
+    }
+
+    //definir a posicao do valor na lista, ou seja, percorrer a lista
+    for (pR = NULL, p = lista; p ; pR = p, p = p->prox) {
+        if (valor < p->valor) {
+            break;
+        }
+    }
+    //no inicio
+    if (p == lista) {
+        novo->prox = lista;
+        lista->ant = novo; //codigo da lista dupla
+        return novo;
+    }
+    //no fim
+    if (!p) {
+        pR->prox = novo;
+        novo->ant = pR; //codigo da lista dupla
+        return lista;
+    }
+    //no meio
+    pR->prox = novo;
+    novo->ant = pR; //codigo da lista dupla
+    novo->prox = p;
+    p->ant = novo;  //codigo da lista dupla
     return lista;
 }
