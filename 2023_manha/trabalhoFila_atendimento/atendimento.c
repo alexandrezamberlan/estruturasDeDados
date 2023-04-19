df07d0d6f60c04d05f1a28ad9f53b360815100c3
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2023-04-19 09:27:17 -0300

    Add files via upload

--- 2023_manha/trabalhoFila_atendimento/atendimento.c
+++ 2023_manha/trabalhoFila_atendimento/atendimento.c
@@ -13,7 +13,9 @@ int main() {
     inicializacao(&filaNormal);
     inicializacao(&filaPrioritaria);
     int opcao;
+    Celula ficha;
     do {
+        system("cls");
         printf("1 - Ficha normal\n");
         printf("2 - Ficha prioritaria\n");
         printf("3 - Chama ficha\n");
@@ -23,10 +25,42 @@ int main() {
 
         switch (opcao) {
             case 1:
+                //exibir a ficha ou imprimir a ficha
+                printf("Ficha normal gerada: %d\n", contadorFilaNormal);
+                inserirFila(contadorFilaNormal, &filaNormal);
+                contadorFilaNormal++;
                 break;
             case 2:
+                //exibir a ficha ou imprimir a ficha
+                printf("Ficha prioritaria gerada: %d\n", contadorFilaPrioritaria);
+                inserirFila(contadorFilaPrioritaria, &filaPrioritaria);
+                contadorFilaPrioritaria++;
                 break;
             case 3:
+                //definir qual fila atender
+                if (contadorAtendimentos % 3 == 0) {
+                    ficha = removerFila(&filaPrioritaria);
+                    if (ficha.conteudo != -27) {
+                        printf("Chamando ficha prioritaria: %d - %02d/%02d/%d %02d:%02d\n", ficha.conteudo, ficha.dataHora.tm_mday, ficha.dataHora.tm_mon + 1, ficha.dataHora.tm_year + 1900, ficha.dataHora.tm_hour, ficha.dataHora.tm_min);
+                        contadorAtendimentos++;
+                    } else {
+                        if (filaNormal.cabeca != NULL) {
+                            ficha = removerFila(&filaNormal);
+                            printf("Chamando ficha normal: %d - %02d/%02d/%d %02d:%02d\n", ficha.conteudo, ficha.dataHora.tm_mday, ficha.dataHora.tm_mon + 1, ficha.dataHora.tm_year + 1900, ficha.dataHora.tm_hour, ficha.dataHora.tm_min);
+                        }
+                    }
+                } else {
+                    ficha = removerFila(&filaNormal);
+                    if (ficha.conteudo != -27) {
+                        printf("Chamando ficha normal: %d - %02d/%02d/%d %02d:%02d\n", ficha.conteudo, ficha.dataHora.tm_mday, ficha.dataHora.tm_mon + 1, ficha.dataHora.tm_year + 1900, ficha.dataHora.tm_hour, ficha.dataHora.tm_min);
+                        contadorAtendimentos++;
+                    } else {
+                        if (filaPrioritaria.cabeca != NULL) {
+                            ficha = removerFila(&filaPrioritaria);
+                            printf("Chamando ficha prioritaria: %d - %02d/%02d/%d %02d:%02d\n", ficha.conteudo, ficha.dataHora.tm_mday, ficha.dataHora.tm_mon + 1, ficha.dataHora.tm_year + 1900, ficha.dataHora.tm_hour, ficha.dataHora.tm_min);
+                        }
+                    }
+                }
                 break;
             case 4:
                 break;
@@ -34,7 +68,7 @@ int main() {
                 printf("Opcao invalida!\n");
                 break;
         }
-
+        system("pause");
     } while (opcao != 4);
 
     return 1;
