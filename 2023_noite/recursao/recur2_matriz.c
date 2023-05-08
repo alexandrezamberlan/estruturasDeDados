99012ee4396cb0772a6ed5a788ebb2fc76dd1e15
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2023-05-08 20:42:19 -0300

    Add files via upload

---
+++ 2023_noite/recursao/recur2_matriz.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAML 4
+#define TAMC 3
+
+void popular(int matriz[TAML][TAMC], int lin, int col) {
+    for (int i = 0; i < lin; i++) {
+        for (int j = 0; j < col; j++) {
+            matriz[i][j] = rand() % 50;
+        }
+    }
+}
+
+void popularR(int matriz[TAML][TAMC], int lin, int col, int backLin, int backcol)
+{
+    if (col > 0) {
+        matriz[lin - 1][col - 1] = rand() % 10;
+        popularR(matriz, lin, col - 1, backLin, backcol);
+    } else if (lin > 0) {
+        popularR(matriz, lin - 1, backcol ,backLin, backcol);
+    }
+}
+
+void inicializarR(int matriz[TAML][TAMC], int lin, int col, int backLin, int backcol)
+{
+    if (col > 0) {
+        matriz[lin - 1][col - 1] = 0;
+        inicializarR(matriz, lin, col - 1, backLin, backcol);
+    } else if (lin > 0) {
+        inicializarR(matriz, lin - 1, backcol ,backLin, backcol);
+    }
+}
+
+void preecherR(int matriz[TAML][TAMC], int lin, int col, int backLin, int backcol)
+{
+    if (col > 0) {
+        if (lin + col == TAML) {
+            matriz[lin - 1][col - 1] = 0;
+        }
+        preencherR(matriz, lin, col - 1, backLin, backcol);
+    } else if (lin > 0) {
+        preencherR(matriz, lin - 1, backcol ,backLin, backcol);
+    }
+}
+
+void exibirR(int matriz[TAML][TAMC], int lin, int col, int backLin, int backcol) {
+    if (col > 0) {
+        exibirR(matriz, lin, col - 1, backLin, backcol);
+        printf("%d\t",matriz[lin - 1][col - 1]);
+    } else if (lin > 0) {
+        exibirR(matriz, lin - 1, backcol ,backLin, backcol);
+        printf("\n");
+    }
+}
+
+void exibir(int matriz[TAML][TAMC], int lin, int col) {
+    for (int i = 0; i < lin; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d\t",matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matriz[TAML][TAMC];
+    srand(time(NULL));
+    // popular(matriz, TAM, TAM);
+    // exibir(matriz, TAM, TAM);
+                    //qtdlin, qtdcol, backuli, backucol
+    inicializarR(matriz, TAML, TAMC, TAML, TAMC);
+    preencherR(matriz, TAML, TAMC, TAML, TAMC);
+    exibirR(matriz, TAML, TAMC, TAML, TAMC);
+    return 1;
+}
