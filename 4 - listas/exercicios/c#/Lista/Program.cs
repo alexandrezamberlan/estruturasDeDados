5ed4ca57a482b40a80bb967d51a440b65d8519c7
Your Name <you@example.com>
2024-03-22 10:05:52 -0300

    c# intersect e except

---
+++ 4 - listas/exercicios/c#/Lista/Program.cs
@@ -0,0 +1,35 @@
+﻿using Lista;
+using System;
+using System.Collections;
+
+List<int> lista1 = new List<int>();
+List<int> lista2 = new List<int>();
+List<int> listaComuns = new List<int>();
+List<int> listaDisjuncao = new List<int>();
+
+Util objeto = new Util();
+
+objeto.popularLista(lista1, 8);
+objeto.popularLista(lista2, 8);
+
+objeto.exibirLista("Lista 1", lista1);
+objeto.exibirLista("Lista 2", lista2);
+
+listaComuns.AddRange(lista1.Intersect(lista2));
+objeto.exibirLista("Números comuns nas duas listas",listaComuns);
+
+listaDisjuncao.AddRange(lista1.Except(lista2));
+listaDisjuncao.AddRange(lista2.Except(lista1));
+objeto.exibirLista("Números em uma das listas", listaDisjuncao);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 4 - listas/exercicios/c#/Lista/Util.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    internal class Util
+    {
+        public void popularLista(List<int> lista, int n)
+        {
+            int valor;
+            Random gerador = new Random();
+            int i = 0;
+            while (i < n)
+            {
+                valor = gerador.Next(20);
+                if (!lista.Contains(valor))
+                {
+                    lista.Add(valor);
+                    lista.Sort();
+                    i++;
+                }
+            }
+        }
+
+        public void exibirLista(String frase, List<int> lista)
+        {
+            Console.WriteLine(frase);
+
+            foreach (var item in lista)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        
+
+        public void localizarDisjuncao(List<int> lista_resultado, List<int> listaA,
+                                                                  List<int> listaB)
+        {
+
+            Boolean localizado;
+            foreach (var item1 in listaA)
+            {
+                localizado = false;
+                foreach (var item2 in listaB)
+                {
+                    if (item1 == item2)
+                    {
+                        localizado = true;
+                        break;
+                    }
+                }
+                if (!localizado)
+                {
+                    lista_resultado.Add(item1);
+                }
+            }
+
+            foreach (var item2 in listaB)
+            {
+                localizado = false;
+                foreach (var item1 in listaA)
+                {
+                    if (item1 == item2)
+                    {
+                        localizado = true;
+                        break;
+                    }
+                }
+                if (!localizado)
+                {
+                    lista_resultado.Add(item2);
+                }
+            }
+
+            lista_resultado.Sort();
+        }
+    }
+}
